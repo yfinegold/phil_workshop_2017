4af5e019177de604ade69d975264e1737de00a6a
Yelena <yelena.finegold@fao.org>
2017-05-24 05:43:33 +0800

    Add files via upload

--- scripts/0_download_data.R
+++ scripts/0_download_data.R
@@ -1,13 +1,9 @@
 ## download the data
 ### click on SOURCE
-
-
-
-
 ####################################################################################
 #######    object: SETUP YOUR LOCAL PARAMETERS                  ####################
 #######    Update : 2017/05/23                                  ####################
-#######    contact: yelena.finegold@fao.org                      ####################
+#######    contact: yelena.finegold@fao.org                      ###################
 ####################################################################################
 
 ####################################################################################
@@ -30,22 +26,13 @@ library(foreign)
 library(dplyr)
 
 ############### DOWNLOAD WORKSHOP DATA
-# workshop_folder <- "C:/Users/finegold/Desktop" 
-# if(!file.exists(file.path(workshop_folder,"workshop_PHL_2017.zip"))){
-#   download.file("https://www.dropbox.com/sh/kvambfjgcil2f79/AACDW66-t97Vf7f_pO4fiUKQa?dl=1",
-#                 file.path(workshop_folder,"workshop_PHL_2017.zip"),"auto",mode="wb")
-#   unzip(file.path(workshop_folder,"workshop_PHL_2017.zip"),exdir=file.path(workshop_folder,"workshop_PHL_2017"))
-#   unzip(file.path(paste0(workshop_folder,"workshop_PHL_2017/data/"),'landcover_2003_2010_IPCC.zip'),exdir=file.path(paste0(workshop_folder,"workshop_PHL_2017/data"),'landcover_2003_2010_IPCC.zip'))
-#   
-# }
-
 ## Loading the input data for analysis ####
 ## Linux version
 
-list <- list.files("~/workshop_PHL_2017/data")
+list <- list.files("~/phil_workshop_2017/data")
 is.na(list)
-if(length(list)==0){
-  setwd('~/workshop_PHL_2017/data')
+if(length(list)<=1){
+  setwd('~/phil_workshop_2017/data')
   system("wget https://www.dropbox.com/s/v9f11umprbxpr9c/landcover_2003_2010_IPCC.zip?dl=0")
   system('mv landcover_2003_2010_IPCC.zip?dl=0 landcover_2003_2010_IPCC.zip')
   system('unzip landcover_2003_2010_IPCC.zip')
@@ -54,7 +41,7 @@ if(length(list)==0){
 
 
 ############### SET WORKING ENVIRONMENT
-rootdir <- paste0('~/workshop_PHL_2017/data')
+rootdir <- paste0('~/phil_workshop_2017/data')
 
 
 setwd(rootdir)
